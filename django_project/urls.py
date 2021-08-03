7cce7484ad2d795f96a3c5ed12189159bf37cc40
sytab072 <20183161@vision.hoseo.edu>
2021-08-03 15:23:30 +0900

    Headpage , about_me page 생성

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('board', include('board.urls')),
+    path('board/', include('board.urls')),
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
 ]
