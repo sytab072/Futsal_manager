0d492e40b0f2ea09d3dd6d73e7f43fa7c1d141af
sytab072 <20183161@vision.hoseo.edu>
2021-08-05 08:25:50 +0900

    post_detailed 테스트-bootstrap navbar미해결

--- board/tests.py
+++ board/tests.py
@@ -48,3 +48,36 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         # 이제 더 이상 게시물이 존재하지않는다는 문구는 보이지 않음.
         self.assertNotIn('이제 더 이상 게시물이 존재하지 않습니다', main_area.text)
+
+    def test_post_detail(self):
+        # 포스트가 존재함.
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world',
+        )
+        # 포스트의 url = '/board/1/'
+        self.assertEqual(post_001.get_absolute_url(), '/board/1/')
+
+        # 상세페이지 테스트(첫번째 페이지)
+        # url로 접근하면 정상적으로 작동함을 확인할 수 있음.(status code: 200)
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # post_list페이지와 같은 navbar가 존재함.
+        navbar = soup.nav
+        self.assertIn('Board', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        # 포스트 제목을 웹 브라우저 탭의 title에서 확인가능
+        self.assertIn(post_001.title, soup.title.text)
+
+        # 포스트 제목이 포스트 영역에 존재
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+
+        # 포스트 영역에 author 확인가능(미구현)
+
+        # 포스트 영역에 content-내용을 확인가능
+        self.assertIn(post_001.content, post_area.text)
