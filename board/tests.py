a6fd82e4040d39850cb477517222656d1c7dbd76
sytab072 <20183161@vision.hoseo.edu>
2021-08-06 15:06:55 +0900

    post_detailed에 categories(category)기능 추가

--- board/tests.py
+++ board/tests.py
@@ -55,14 +55,8 @@ class TestView(TestCase):
     def category_card_test(self, soup):
         categories_card = soup.find('div', id='categories-card')
         self.assertIn('Categories', categories_card.text)
-        self.assertIn(
-            f'{self.category_programming.name} ({self.category_programming.post_set.count()})',
-            categories_card.text
-        )
-        self.assertIn(
-            f'{self.category_music.name} ({self.category_music.post_set.count()})',
-            categories_card.text
-        )
+        self.assertIn(f'{self.category_programming.name} ({self.category_programming.post_set.count()})', categories_card.text)
+        self.assertIn(f'{self.category_music.name} ({self.category_music.post_set.count()})', categories_card.text)
         self.assertIn(f'미분류 (1)', categories_card.text)
 
 
@@ -106,33 +100,31 @@ class TestView(TestCase):
 
     def test_post_detail(self):
         # 포스트가 존재함.
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.',
-            content='Hello World. We are the world',
-            author=self.user_kim,
-        )
+
         # 포스트의 url = '/board/1/'
-        self.assertEqual(post_001.get_absolute_url(), '/board/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/board/1/')
 
         # 상세페이지 테스트(첫번째 페이지)
         # url로 접근하면 정상적으로 작동함을 확인할 수 있음.(status code: 200)
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # post_list페이지와 같은 navbar가 존재함.
         self.navbar_test(soup)
+        self.category_card_test(soup)
 
         # 포스트 제목을 웹 브라우저 탭의 title에서 확인가능
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         # 포스트 제목이 포스트 영역에 존재
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
 
         # 포스트 영역에 author 확인가능(구현)
         self.assertIn(self.user_kim.username.upper(), post_area.text)
 
         # 포스트 영역에 content-내용을 확인가능
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
--- board/views.py
+++ board/views.py
@@ -6,6 +6,7 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
@@ -15,6 +16,12 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 # Create your views here.
 
 # def index(request):
