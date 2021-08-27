2939e7b3217866d6ee71725f5cf6aba517741d82
sytab072 <20183161@vision.hoseo.edu>
2021-08-27 23:49:12 +0900

    Comments in Post_detail

--- board/admin.py
+++ board/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 # Register your models here.
 
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
